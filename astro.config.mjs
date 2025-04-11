49264fe4e2acfab93a0b9a693bef3eb40e7c0ec9
Alterr <markpav383@gmail.com>
2025-04-11 17:06:43 +0300

    astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://cherry-editor.github.io/site/',
-  base: "",
+  base: "/site/",
   integrations: [mdx(), sitemap(), tailwind()],
   output: "static"
 });
