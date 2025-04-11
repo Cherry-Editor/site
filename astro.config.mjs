b6ce912001549235bbe7e15fd5721c40d5c39600
Alterr <markpav383@gmail.com>
2025-04-11 17:32:17 +0300

    astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,15 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
+import sharp from "astro/assets/services/sharp";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://cherry-editor.github.io/site/',
   base: "/site/",
   integrations: [mdx(), sitemap(), tailwind()],
-  output: "static"
+  output: "static",
+  image: {
+    service: sharp,
+  }
 });
